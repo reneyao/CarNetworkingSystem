a6efde6f661e3c98f9e6865a1ff4fbc527aa048e
reneyao <reneyao710@163.com>
2025-04-18 17:52:47 +0800

    增加一个检验的controller

---
+++ VehicleNetworkWeb/src/main/java/cn/itcast/dataservice/controller/HelloController.java
@@ -0,0 +1,16 @@
+package cn.itcast.dataservice.controller;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    @ApiOperation(value = "检测是否正常启动")
+    public String helloWorld() {
+        return "Hello World";
+    }
+}
